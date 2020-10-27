9e4c60646adb7c755cb011d95ee2eaf6caa3b7a9
ApiwatHantrakool(Nekky) <apiwat@simyasolutions.com>
2020-10-27 17:51:46 +0700

    Add example 01 and 02

---
+++ src/js/examples/Example02.js
@@ -0,0 +1,16 @@
+export const getTxtCourtNumberFromCourtList = (courtList) => {
+  if (courtList === undefined || courtList.length === 0) {
+    return "";
+  } else {
+    const integerNumberCourtArr = courtList.map((val) => {
+      return getTxtCourtNumberFromCourtId(val);
+    });
+    const txtCourtNumber = integerNumberCourtArr.join(",");
+    return txtCourtNumber;
+  }
+};
+
+export const getTxtCourtNumberFromCourtId = (courtId) => {
+  const result = courtId.replace("c", "");
+  return parseInt(result) + 1;
+};
---
+++ src/js/examples/Example02.spec.js
@@ -0,0 +1,47 @@
+import {
+  getTxtCourtNumberFromCourtList,
+  getTxtCourtNumberFromCourtId,
+} from "./Example02";
+
+describe("get text of court number from array list of court numbers", () => {
+  it("case: normal", () => {
+    // Arrange
+    const courtList = ["c00", "c01", "c02"];
+    // Act
+    const actualResult = getTxtCourtNumberFromCourtList(courtList);
+    // Assert
+    const expectResult = "1,2,3";
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it("case: empty list", () => {
+    // Arrange
+    const courtList = [];
+    // Act
+    const actualResult = getTxtCourtNumberFromCourtList(courtList);
+    // Assert
+    const expectResult = "";
+    expect(actualResult).toEqual(expectResult);
+  });
+});
+
+describe("get text of court number from court ID", () => {
+  it("case: normal", () => {
+    // Arrange
+    const courtId = "c00";
+    // Act
+    const actualResult = getTxtCourtNumberFromCourtId(courtId);
+    // Assert
+    const expectResult = 1;
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it("case: empty list", () => {
+    // Arrange
+    const courtId = "";
+    // Act
+    const actualResult = getTxtCourtNumberFromCourtId(courtId);
+    // Assert
+    expect(actualResult).toBeNaN();
+  });
+});
