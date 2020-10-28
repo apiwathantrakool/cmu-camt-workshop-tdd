12a6aa722554197499cacc3f6bf28db4f1899a3a
ApiwatHantrakool <apiwat@simyasolutions.com>
2020-10-28 22:28:04 +0700

    Add more exercise

--- src/js/exercises/Exercise03.js
+++ src/js/exercises/Exercise03.js
@@ -1,38 +1,15 @@
-import moment from "moment";
-
-const getDateFormat = (date) => {
-  return moment(date).format("YYYY-MM-DD");
+export const getStatusBackgroundColor = (remainHours, totalHours) => {
+  const hoursPercentage = parseInt(getHoursPercentage(remainHours, totalHours));
+  if (hoursPercentage > 40) {
+    return 'blue';
+  } else if (hoursPercentage <= 40 && hoursPercentage > 0) {
+    return 'yellow';
+  } else {
+    return 'red';
+  }
 };
 
-export const getPreviousDateList = (numberOfDate, startDate) => {
-  const dateList = [];
-  for (let i = 1; i <= numberOfDate; i++) {
-    var date = moment(startDate).subtract(i, "days");
-    const dateFormat = getDateFormat(moment(date));
-    dateList.push(dateFormat);
-  }
-  return dateList;
-};
-
-// export const getDateListFromFilter = (filterId, startDate) => {
-//   const dateList = {
-//     today: [startDate],
-//     sevenDays: getPreviousDateList(7, startDate),
-//     thirtyDays: getPreviousDateList(30, startDate),
-//     default: [],
-//   };
-//   return dateList[filterId] || dateList["default"];
-// };
-
-export const getDateListFromFilter = (filterId, startDate) => {
-  switch (filterId) {
-    case "today":
-      return [startDate];
-    case "sevenDays":
-      return getPreviousDateList(7, startDate);
-    case "thirtyDays":
-      return getPreviousDateList(30, startDate);
-    default:
-      return [];
-  }
+export const getHoursPercentage = (hours, totalHours) => {
+  const hoursPercentage = (parseInt(hours) / parseInt(totalHours)) * 100;
+  return hoursPercentage;
 };
