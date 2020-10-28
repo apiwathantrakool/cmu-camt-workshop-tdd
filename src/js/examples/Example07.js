bcf8ee5ec28b6f08e3b0fc9de99d93ade9a6b75f
ApiwatHantrakool(Nekky) <apiwat@simyasolutions.com>
2020-10-28 12:44:16 +0700

    Add example 07

---
+++ src/js/examples/Example07.js
@@ -0,0 +1,20 @@
+import _ from "lodash";
+
+export const getInitialSelectionCourtList = (
+  courtList,
+  prevSelectedCourtList
+) => {
+  _.forEach(courtList, (val, key) => {
+    const isPrevSelected = _.get(prevSelectedCourtList, key) ? true : false;
+    const isAnotherBooking = _.get(val, "bookingId") ? true : false;
+    const courtStatus = _.get(val, "status", "");
+    const isCourtClosed = _.isEqual("closed", courtStatus) ? true : false;
+    const isDisabled = isAnotherBooking || isCourtClosed;
+    courtList[key] = {
+      ...val,
+      isSelected: isPrevSelected,
+      isDisabled: isDisabled,
+    };
+  });
+  return courtList;
+};
