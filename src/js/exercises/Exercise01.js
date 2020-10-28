5fc19201e69ce302096fc61f98edbcfc287b95b9
ApiwatHantrakool(Nekky) <apiwat@simyasolutions.com>
2020-10-28 19:21:24 +0700

    Add Exercise01 desciption

--- src/js/exercises/Exercise01.js
+++ src/js/exercises/Exercise01.js
@@ -1,3 +1,14 @@
+// Requirements: create a function to convert list of IDs to text number.
+// 1. receive array of IDs.
+// 2. remove character "c" then plus +1 for each ID.
+// 3. Join the IDs together by "-" sign.
+// Example:
+// input: ["c00", "c01", "c02"]
+// output: 1-2-3
+// Example:
+// input: ["c00"]
+// output: 1
+
 export const getTxtCourtNumberFromCourtList = (courtList) => {
   if (courtList === undefined || courtList.length === 0) {
     return "";
