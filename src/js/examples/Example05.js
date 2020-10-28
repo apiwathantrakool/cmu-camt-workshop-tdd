f011f2c8706a540d36ef062dee0626f2b841f62e
ApiwatHantrakool(Nekky) <apiwat@simyasolutions.com>
2020-10-28 10:59:08 +0700

    Update Example 05

--- src/js/examples/Example05.js
+++ src/js/examples/Example05.js
@@ -1,37 +1,12 @@
-export const separateBookingDate = (
-  bookingList: Object,
-  maxDateFormat: string
-) => {
-  const bookingListWithCreateAt = {};
-  _.forEach(bookingList, (val, key) => {
-    const createAt = _.get(val, KEY_CREATE_AT, 0);
-    const createAtDateFormat = getDateFormatFromMilliseconds(
-      createAt,
-      DEFAULT_DATE_FORMAT
-    );
-    bookingListWithCreateAt[key] = { ...val, createAtDateFormat };
-  });
-  const bookingDataDateGroup = _.groupBy(
-    bookingListWithCreateAt,
-    KEY_CREATE_AT_DATE_FORMAT
+import _ from "lodash";
+
+export const separateBookingDate = (bookingList, maxDateFormat) => {
+  const bookingListCreateDateGroup = _.groupBy(bookingList, "createDate");
+  const bookingListCreateDateGroupData = _.map(
+    bookingListCreateDateGroup,
+    (data, id) => {
+      return { id, data };
+    }
   );
-  const separatedBookingDate = _.reduce(
-    bookingDataDateGroup,
-    (result, val, key) => {
-      if (key <= maxDateFormat) {
-        result[key] = {
-          id: key,
-          data: val,
-        };
-      }
-      return result;
-    },
-    {}
-  );
-  const separatedBookingDateSort = _.reverse(
-    _.sortBy(separatedBookingDate, (o) => {
-      return o.id;
-    })
-  );
-  return separatedBookingDateSort;
+  return bookingListCreateDateGroupData;
 };
--- src/js/examples/Example05.spec.js
+++ src/js/examples/Example05.spec.js
@@ -1,31 +1,25 @@
-describe("separateBookingDate", () => {
-  it("get booking list with separate dates", () => {
+import { separateBookingDate } from "./Example05";
+
+describe("return booking array object group by createDate", () => {
+  it("case: normal", () => {
     const bookingData = {
-      // 2020-07-03
       b001: {
-        status: STATUS.VERIFYING,
-        createAt: 1593739256789,
+        createDate: "2020-07-03",
         bookingId: "b001",
         totalPrice: 100,
       },
-      // 2020-07-08
       b002: {
-        status: STATUS.CONFIRMED,
-        createAt: 1594183318858,
+        createDate: "2020-07-08",
         bookingId: "b002",
         totalPrice: 200,
       },
-      // 2020-07-08
       b003: {
-        status: STATUS.CANCELED,
-        createAt: 1594183318858,
+        createDate: "2020-07-08",
         bookingId: "b003",
         totalPrice: 300,
       },
-      // 2020-07-03
       b004: {
-        status: STATUS.CANCELED,
-        createAt: 1593739256789,
+        createDate: "2020-07-03",
         bookingId: "b004",
         totalPrice: 400,
       },
@@ -34,42 +28,18 @@ describe("separateBookingDate", () => {
     const result = separateBookingDate(bookingData, today);
     const expectResult = [
       {
+        id: "2020-07-03",
         data: [
-          {
-            status: STATUS.CONFIRMED,
-            createAt: 1594183318858,
-            bookingId: "b002",
-            createAtDateFormat: "2020-07-08",
-            totalPrice: 200,
-          },
-          {
-            status: STATUS.CANCELED,
-            createAt: 1594183318858,
-            bookingId: "b003",
-            createAtDateFormat: "2020-07-08",
-            totalPrice: 300,
-          },
+          { createDate: "2020-07-03", bookingId: "b001", totalPrice: 100 },
+          { createDate: "2020-07-03", bookingId: "b004", totalPrice: 400 },
         ],
-        id: "2020-07-08",
       },
       {
+        id: "2020-07-08",
         data: [
-          {
-            status: STATUS.VERIFYING,
-            createAt: 1593739256789,
-            bookingId: "b001",
-            createAtDateFormat: "2020-07-03",
-            totalPrice: 100,
-          },
-          {
-            status: STATUS.CANCELED,
-            createAt: 1593739256789,
-            bookingId: "b004",
-            createAtDateFormat: "2020-07-03",
-            totalPrice: 400,
-          },
+          { createDate: "2020-07-08", bookingId: "b002", totalPrice: 200 },
+          { createDate: "2020-07-08", bookingId: "b003", totalPrice: 300 },
         ],
-        id: "2020-07-03",
       },
     ];
     expect(result).toEqual(expectResult);
