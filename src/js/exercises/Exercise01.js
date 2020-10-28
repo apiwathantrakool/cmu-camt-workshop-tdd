15d347a417d8a92200db4ee1efea8d12a510870a
ApiwatHantrakool <apiwat@simyasolutions.com>
2020-10-28 23:25:26 +0700

    Add one more test case

--- src/js/exercises/Exercise01.js
+++ src/js/exercises/Exercise01.js
@@ -13,13 +13,13 @@
 
 export const getTxtCourtNumberFromCourtList = (courtList) => {
   if (courtList === undefined || courtList.length === 0) {
-    return "";
+    return '';
   } else {
     const integerNumberCourtArr = courtList.map((val) => {
-      const result = val.replace("c", "");
+      const result = val.replace('c', '');
       return parseInt(result) + 1;
     });
-    const txtCourtNumber = integerNumberCourtArr.join("-");
+    const txtCourtNumber = integerNumberCourtArr.join('-');
     return txtCourtNumber;
   }
 };
