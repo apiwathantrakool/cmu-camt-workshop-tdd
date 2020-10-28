fbf4a57b60444d9298e0d586f9e7cf8862b00fe7
ApiwatHantrakool <apiwat@simyasolutions.com>
2020-10-28 23:10:00 +0700

    Update exercise 03

--- src/js/exercises/Exercise03.js
+++ src/js/exercises/Exercise03.js
@@ -1,10 +1,23 @@
-export const getStatusBackgroundColor = (remainHours, totalHours) => {
+// Requirements: create a function to return color value following percentage range.
+// 1. Receive remain hours and total hours.
+// 2. Calculate percentage from remain hours and total hours.
+// 3. Add condition to return color value following:
+// 3.1 More than or equal 80% = 'green'
+// 3.2 79% to 60% = 'blue'
+// 3.3 59% to 30% = 'yellow'
+// 3.4 Less than or equal 29% = 'red'
+
+// Optional: create a function to calculate percentage and unit test
+
+export const getColorByHoursPercentage = (remainHours, totalHours) => {
   const hoursPercentage = parseInt(getHoursPercentage(remainHours, totalHours));
-  if (hoursPercentage > 40) {
+  if (hoursPercentage >= 80) {
+    return 'green';
+  } else if (hoursPercentage < 80 && hoursPercentage >= 60) {
     return 'blue';
-  } else if (hoursPercentage <= 40 && hoursPercentage > 0) {
+  } else if (hoursPercentage < 60 && hoursPercentage >= 30) {
     return 'yellow';
-  } else {
+  } else if (hoursPercentage <= 29) {
     return 'red';
   }
 };
--- src/js/exercises/Exercise03.spec.js
+++ src/js/exercises/Exercise03.spec.js
@@ -1,56 +1,133 @@
-import { getStatusBackgroundColor, getHoursPercentage } from './Exercise03';
+import { getColorByHoursPercentage, getHoursPercentage } from './Exercise03';
 
-describe('get backgroundColor style value for status bar by percentage', () => {
-  it('case: percentage > 40', () => {
+describe('get color value following percentage range', () => {
+  it('case: percentage > 80', () => {
     // Arrange
-    const remainHours = 90;
+    const remainHours = 100;
     const totalHours = 100;
     // Act
-    const actualResult = getStatusBackgroundColor(remainHours, totalHours);
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'green';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: percentage === 80', () => {
+    // Arrange
+    const remainHours = 80;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'green';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: percentage === 79', () => {
+    // Arrange
+    const remainHours = 79;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
     // Assert
     const expectResult = 'blue';
     expect(actualResult).toEqual(expectResult);
   });
 
-  it('case: percentage === 40', () => {
+  it('case: percentage < 79 and percentage > 60', () => {
     // Arrange
-    const remainHours = 40;
+    const remainHours = 70;
     const totalHours = 100;
     // Act
-    const actualResult = getStatusBackgroundColor(remainHours, totalHours);
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'blue';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: percentage === 60', () => {
+    // Arrange
+    const remainHours = 60;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'blue';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: percentage === 59', () => {
+    // Arrange
+    const remainHours = 59;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
     // Assert
     const expectResult = 'yellow';
     expect(actualResult).toEqual(expectResult);
   });
 
-  it('case: percentage < 40', () => {
+  it('case: percentage < 59 and percentage > 30', () => {
     // Arrange
-    const remainHours = 39;
+    const remainHours = 50;
     const totalHours = 100;
     // Act
-    const actualResult = getStatusBackgroundColor(remainHours, totalHours);
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
     // Assert
     const expectResult = 'yellow';
     expect(actualResult).toEqual(expectResult);
   });
 
-  it('case: percentage === 0', () => {
+  it('case: percentage === 30', () => {
     // Arrange
-    const remainHours = 39;
+    const remainHours = 30;
     const totalHours = 100;
     // Act
-    const actualResult = getStatusBackgroundColor(remainHours, totalHours);
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
     // Assert
     const expectResult = 'yellow';
     expect(actualResult).toEqual(expectResult);
   });
 
-  it('case: percentage < 0', () => {
+  it('case: percentage === 29', () => {
+    // Arrange
+    const remainHours = 29;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'red';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: percentage < 29', () => {
+    // Arrange
+    const remainHours = 20;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'red';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: zero', () => {
+    // Arrange
+    const remainHours = 0;
+    const totalHours = 100;
+    // Act
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
+    // Assert
+    const expectResult = 'red';
+    expect(actualResult).toEqual(expectResult);
+  });
+
+  it('case: negative', () => {
     // Arrange
     const remainHours = -1;
     const totalHours = 100;
     // Act
-    const actualResult = getStatusBackgroundColor(remainHours, totalHours);
+    const actualResult = getColorByHoursPercentage(remainHours, totalHours);
     // Assert
     const expectResult = 'red';
     expect(actualResult).toEqual(expectResult);
