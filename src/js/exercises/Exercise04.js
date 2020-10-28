0d8860775ec04fe44895aa52cbd874bde6adfbc3
ApiwatHantrakool <apiwat@simyasolutions.com>
2020-10-28 23:15:54 +0700

    Update Exercise 04

--- src/js/exercises/Exercise04.js
+++ src/js/exercises/Exercise04.js
@@ -1,13 +1,18 @@
-import moment from "moment";
+// Requirements: refactor function "getDateListFromFilter".
+// 1. Replace switch case logic with object logic.
+// 2. Run the test without update anything.
+// ref: https://ultimatecourses.com/blog/deprecating-the-switch-statement-for-object-literals
+
+import moment from 'moment';
 
 const getDateFormat = (date) => {
-  return moment(date).format("YYYY-MM-DD");
+  return moment(date).format('YYYY-MM-DD');
 };
 
 export const getPreviousDateList = (numberOfDate, startDate) => {
   const dateList = [];
   for (let i = 1; i <= numberOfDate; i++) {
-    var date = moment(startDate).subtract(i, "days");
+    var date = moment(startDate).subtract(i, 'days');
     const dateFormat = getDateFormat(moment(date));
     dateList.push(dateFormat);
   }
@@ -26,11 +31,11 @@ export const getPreviousDateList = (numberOfDate, startDate) => {
 
 export const getDateListFromFilter = (filterId, startDate) => {
   switch (filterId) {
-    case "today":
+    case 'today':
       return [startDate];
-    case "sevenDays":
+    case 'sevenDays':
       return getPreviousDateList(7, startDate);
-    case "thirtyDays":
+    case 'thirtyDays':
       return getPreviousDateList(30, startDate);
     default:
       return [];
